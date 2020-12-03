32b17d5ffda4cc0637a135763c16d1bb8ac6ee0e
Eiji Otsuki <bigoonage0104@gmail.com>
2020-12-03 22:58:25 +0900

    Add words

--- js.main.js
+++ js.main.js
@@ -2,16 +2,33 @@
 
 {
   function setWord() {
-    word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
+    data = words.splice(Math.floor(Math.random() * words.length), 1)[0];
+    word = data[0];
+    explanation = data[1];
     target.textContent = word;
     loc = 0;
   }
 
+  function setExplanation() {
+    let text = document.getElementById('explanation')
+    text.innerText = explanation;
+  }
+
   const words = [
-    'splice','repeat','keydown','length','aleat','script','console','return', 'use strict',
-    'prompt','Polymophism','getElementById','function', 'eval', 'parseInt', 'string', 
+    ['kimetsunoyaiba', '鬼滅の刃'],
+    ['jujutukaisenn', '呪術廻戦'],
+    ['dragonball', 'ドラゴンボール'],
+    ['slamdunk', 'スラムダンク'],
+    ['hunterhunter', 'HUNTER×HUNTER'],
+    ['gintama', '銀魂'],
+    ['onepiece', 'ワンピース'],
+    ['naruto', 'ナルト'],
+    ['bokunoheroacademia', '僕のヒーローアカデミア'],
+    ['kotirakatsusikakukamearikouenmaehasyutujo', 'こちら葛飾区亀有公園前派出所'],
   ];
   let word;
+  let data;
+  let explanation;
   let loc = 0;
   let startTime;
   let isPlaying = false;
@@ -25,6 +42,7 @@
     isPlaying = true;
     startTime = Date.now();
     setWord();
+    setExplanation();
   });
 
   document.addEventListener('keydown', e => {
@@ -38,13 +56,15 @@
 
     if (loc === word.length) {
       if (words.length === 0) {
+        document.getElementById('explanation').innerText = '';
         const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
         const result = document.getElementById('result');
         result.textContent = `Finished! ${elapsedTime} seconds!`;
         return;
       }
-
+      
       setWord();
+      setExplanation();
     }
   });
 }
